7d22d270fccef77578864c4960793b35a3e49838
dic09005 <dic09005@byui.edu>
2022-11-17 21:13:38 -0700

    update action

--- cycle/cycle/__main__.py
+++ cycle/cycle/__main__.py
@@ -5,8 +5,7 @@ from game.casting.score import Score
 from game.casting.snake_one import Snake_one
 from game.casting.snake_two import Snake_two
 from game.scripting.script import Script
-from game.scripting.control_actors_action import ControlActorsAction_one
-from game.scripting.control_actors_action import ControlActorsAction_two
+from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
 from game.scripting.handle_collisions_action import HandleCollisionsAction
 from game.scripting.draw_actors_action import DrawActorsAction
@@ -35,8 +34,7 @@ def main():
     video_service = VideoService()
 
     script = Script()
-    script.add_action("input", ControlActorsAction_one(keyboard_service))
-    script.add_action("input", ControlActorsAction_two(keyboard_service))
+    script.add_action("input", ControlActorsAction(keyboard_service))
     script.add_action("update", MoveActorsAction())
     script.add_action("update", HandleCollisionsAction())
     script.add_action("update", ControlGrowing())
--- cycle/cycle/game/scripting/control_actors_action.py
+++ cycle/cycle/game/scripting/control_actors_action.py
@@ -2,8 +2,13 @@ import constants
 from game.scripting.action import Action
 from game.shared.point import Point
 
+RIGHT = Point(constants.CELL_SIZE, 0)
+LEFT = Point(-constants.CELL_SIZE, 0)
+UP = Point(0, -constants.CELL_SIZE)
+DOWN = Point(0, constants.CELL_SIZE)
 
-class ControlActorsAction_one(Action):
+
+class ControlActorsAction(Action):
     """
     An input action that controls the snake number 1.
     
@@ -20,78 +25,47 @@ class ControlActorsAction_one(Action):
             keyboard_service (KeyboardService): An instance of KeyboardService.
         """
         self._keyboard_service = keyboard_service
-        self._direction = Point(constants.CELL_SIZE, 0)
+        self._direction_1 = Point(0, -constants.CELL_SIZE)
+        self._direction_2 = Point(0, -constants.CELL_SIZE)
 
     def execute(self, cast, script):
         """Executes the control actors action.
-
         Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game.
         """
-        # left
-        if self._keyboard_service.is_key_down('a'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+    # Controls for 1st snake         
+        snake_1 = cast.get_actors("snakes")[0]
         
-        # right
-        if self._keyboard_service.is_key_down('d'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+        # Fixed snake trailing back on itself!
+        if self._keyboard_service.is_key_down('a') and snake_1.get_head().get_velocity() != RIGHT:
+            self._direction_1 = LEFT
         
-        # up
-        if self._keyboard_service.is_key_down('w'):
-            self._direction = Point(0, -constants.CELL_SIZE)
+        if self._keyboard_service.is_key_down('d') and snake_1.get_head().get_velocity() != LEFT:
+            self._direction_1 = RIGHT
         
-        # down
-        if self._keyboard_service.is_key_down('s'):
-            self._direction = Point(0, constants.CELL_SIZE)
+        if self._keyboard_service.is_key_down('w') and snake_1.get_head().get_velocity() != DOWN:
+            self._direction_1 = UP
         
-        snake1 = cast.get_first_actor("snakes")
-        snake1.turn_head(self._direction)
+        if self._keyboard_service.is_key_down('s') and snake_1.get_head().get_velocity() != UP:
+            self._direction_1 = DOWN
 
+        snake_1.turn_head(self._direction_1)
 
-# Creating the ControlActorsAction_two class
+    # Controls for 2nd snake
+        snake_2 = cast.get_actors("snakes")[1]
 
-class ControlActorsAction_two(Action):
-    """
-    An input action that controls the snake number 2.
-    
-    The responsibility of ControlActorsAction is to get the direction and move the snake's number two head.
+        # Avoid snake trailing back on itself
+        if self._keyboard_service.is_key_down('j') and snake_2.get_head().get_velocity() != RIGHT:
+            self._direction_2 = LEFT        
 
-    Attributes:
-        _keyboard_service (KeyboardService): An instance of KeyboardService.
-    """
-
-    def __init__(self, keyboard_service):
-        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        if self._keyboard_service.is_key_down('l') and snake_2.get_head().get_velocity() != LEFT:
+            self._direction_2 = RIGHT
         
-        Args:
-            keyboard_service (KeyboardService): An instance of KeyboardService.
-        """
-        self._keyboard_service = keyboard_service
-        self._direction = Point(constants.CELL_SIZE, 0)
-
-    def execute(self, cast, script):
-        """Executes the control actors action.
-
-        Args:
-            cast (Cast): The cast of Actors in the game.
-            script (Script): The script of Actions in the game.
-        """
-        # left
-        if self._keyboard_service.is_key_down('j'):
-            self._direction = Point(-constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('i') and snake_2.get_head().get_velocity() != DOWN:
+            self._direction_2 = UP
         
-        # right
-        if self._keyboard_service.is_key_down('l'):
-            self._direction = Point(constants.CELL_SIZE, 0)
+        if self._keyboard_service.is_key_down('k') and snake_2.get_head().get_velocity() != UP:
+            self._direction_2 = DOWN
         
-        # up
-        if self._keyboard_service.is_key_down('i'):
-            self._direction = Point(0, -constants.CELL_SIZE)
-        
-        # down
-        if self._keyboard_service.is_key_down('k'):
-            self._direction = Point(0, constants.CELL_SIZE)
-        
-        snake2 = cast.get_first_actor("snakes")
-        snake2.turn_head(self._direction)
+        snake_2.turn_head(self._direction_2)
